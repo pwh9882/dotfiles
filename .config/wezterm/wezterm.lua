38799a643a3283f0c3e192f9e2c150865ac67577
pwh9882 <pwh9882@gmail.com>
2025-09-15 14:18:27 +0900

    wezterm mux 추가 (ddps1)

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -549,4 +549,44 @@ end)
 -- save workspace state when switching and restore it when creating new workspaces.
 
 -- Returns our config to be evaluated. We must always do this at the bottom of this file
+
+config.ssh_domains = {
+  {
+    -- The name of this specific domain.  Must be unique amongst
+    -- all types of domain in the configuration file.
+    name = 'ddps',
+
+    -- identifies the host:port pair of the remote server
+    -- Can be a DNS name or an IP address with an optional
+    -- ":port" on the end.
+    remote_address = 'ddpssrv1.ddps.cloud:33021',
+
+    -- Whether agent auth should be disabled.
+    -- Set to true to disable it.
+    -- no_agent_auth = false,
+
+    -- The username to use for authenticating with the remote host
+    username = 'whpark',
+
+    ssh_option = {
+      identityfile = '~/.ssh/ddps-srv-1_ed25519',
+      
+    },
+
+    -- If true, connect to this domain automatically at startup
+    -- connect_automatically = true,
+
+    -- Specify an alternative read timeout
+    -- timeout = 60,
+
+    -- The path to the wezterm binary on the remote host.
+    -- Primarily useful if it isn't installed in the $PATH
+    -- that is configure for ssh.
+    -- remote_wezterm_path = "/home/yourusername/bin/wezterm"
+  },
+}
+
+
+
+
 return config
